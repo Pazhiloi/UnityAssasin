f65642807884ed964e3b28c4ecff305a1cfdb99f
Miroslav Miroslavov <martinsen06@gmail.com>
2023-10-17 19:13:15 +0300

    12 Player Controller Unity 3d Player Face Direction

--- AssasinCreedClone/Assets/Scripts/PlayerController/PlayerScript.cs
+++ AssasinCreedClone/Assets/Scripts/PlayerController/PlayerScript.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-   [Header("Player Movement")]
-   public float movementSpeed = 3f;
+  [Header("Player Movement")]
+  public float movementSpeed = 3f;
 
-   private void Update() {
+  private void Update()
+  {
     PlayerMovement();
-   }
+  }
 
-   void PlayerMovement(){
+  void PlayerMovement()
+  {
     float horizontal = Input.GetAxis("Horizontal");
     float vertical = Input.GetAxis("Vertical");
 
+    float movementAmount = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+
     var movementInput = (new Vector3(horizontal, 0, vertical)).normalized;
 
-    transform.position += movementInput * movementSpeed * Time.deltaTime;
-   }
+    if (movementAmount > 0)
+    {
+      transform.position += movementInput * movementSpeed * Time.deltaTime;
+      transform.rotation = Quaternion.LookRotation(movementInput);
+    }
+  }
 
 }
