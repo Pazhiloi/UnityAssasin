d630508f814cc82d1adf5be84221910e8160e52f
Miroslav Miroslavov <martinsen06@gmail.com>
2023-10-17 18:56:19 +0300

    10. Invert Camera Movement

--- AssasinCreedClone/Assets/Scripts/CameraControllerScripts/MainCameraController.cs
+++ AssasinCreedClone/Assets/Scripts/CameraControllerScripts/MainCameraController.cs
@@ -9,21 +9,28 @@ public class MainCameraController : MonoBehaviour
   public float gap = 3f;
   public float rotSpeed = 3f;
 
-  float rotX;
-  float rotY;
+
+  [Header("Camera handling")]
   public float minVerAngle = -14f;
   public float maxVerAngle = 45f;
   public Vector2 framingBalance;
-
+  float rotX;
+  float rotY;
+  public bool invertX;
+  public bool invertY;
+  float invertXValue;
+  float invertYValue;
   private void Start() {
     Cursor.lockState = CursorLockMode.Locked;
   }
   private void Update()
   {
+    invertXValue = (invertX) ? -1 : 1;
+    invertYValue = (invertY) ? -1 : 1;
 
-    rotX += Input.GetAxis("Mouse Y") * rotSpeed;
+    rotX += Input.GetAxis("Mouse Y") * invertXValue * rotSpeed;
     rotX = Mathf.Clamp(rotX, minVerAngle, maxVerAngle);
-    rotY += Input.GetAxis("Mouse X") * rotSpeed;
+    rotY += Input.GetAxis("Mouse X") * invertYValue * rotSpeed;
 
     var targetRotation = Quaternion.Euler(rotX, rotY, 0);
 
